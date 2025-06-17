a93c49c1d1fce09c2ec774920e0882b824689024
Sebastian Pytka <76405637+Fablek@users.noreply.github.com>
2025-06-17 19:12:39 +0200

    🚑 database migrate if not exist

--- services/FileService/Program.cs
+++ services/FileService/Program.cs
@@ -72,4 +72,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<FileDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
